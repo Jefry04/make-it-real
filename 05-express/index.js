41e92ff7be62bfdbef9481671095c163226853f5
Jefferson <jefry04@gmail.com>
2020-09-23 18:58:29 -0500

    put

--- 05-express/index.js
+++ 05-express/index.js
@@ -38,13 +38,12 @@ app.route("/users")
     res.send(users);
   })
   .post((req, res) => {
-    const user = {
+    let user = {
       name: req.body.name,
       username: req.body.username,
       email: req.body.email,
       password: req.body.password
     };
-
     const findUser = users.find(u => u.username === user.username);
     if (findUser === undefined) {
       users.push(user);
@@ -55,42 +54,50 @@ app.route("/users")
 
   })
 
-  app.route("/users/:username")
+app.route("/users/:username")
   .get((req, res) => {
-      const username = req.params.username;
-      const findUser = users.find(u => u.username === username);
-      if( findUser === undefined ){
-          res.status(500).send("El usuario consultado no existe");
-      }else{
-          res.status(200).send(findUser);
-      }
-  })
- 
-  .delete((req, res) => {
-      const username = req.params.username;
-      const findUser = users.find(u => u.username === username);
-      if( findUser === undefined ){
-          res.status(500).send("El usuario no existe");
-      }else{
-        const result = users.filter(u => u.username !== username);
-        users = result;
-        res.status(200).send(users);
-          
-      }
-  })
-  .put((req, res) => {
     const username = req.params.username;
     const findUser = users.find(u => u.username === username);
-    if( findUser === undefined ){
-        res.status(500).send("El DELETE no existe");
-    }else{
-        users.splice(0);
-        users.push 
-        res.status(200).send(users);
+    if (findUser === undefined) {
+      res.status(500).send("El usuario consultado no existe");
+    } else {
+      res.status(200).send(findUser);
     }
-});
+  })
 
-  
+  .delete((req, res) => {
+    const username = req.params.username;
+    const findUser = users.find(u => u.username === username);
+    if (findUser === undefined) {
+      res.status(500).send("El usuario no existe");
+    } else {
+      const result = users.filter(u => u.username !== username);
+      users = result;
+      res.status(200).send(users);
+
+
+    }
+  })
+  .put((req, res) => {
+    const username1 = req.params.username;
+    let findUser = users.find(u => u.username === username1);
+    if (findUser === undefined) {
+      res.status(500).send("Elusuario no existe");
+    } else {
+      let user = {
+        name: req.body.name,
+        email: req.body.email,
+        username: username1,
+        password: req.body.password
+      };
+      let pos = users.findIndex(x => x.username === username1);
+      users.splice(pos, 1, user);
+
+      res.send(users);
+    }
+  });
+
+
 
 
 

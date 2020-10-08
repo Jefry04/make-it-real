6c22b54eb5a6e8c91234f02a08cd2d4c19cf8de7
Jefferson <jefry04@gmail.com>
2020-10-07 21:54:39 -0500

    audit and morgan

--- 05-express/api/routers/users/index.js
+++ 05-express/api/routers/users/index.js
@@ -3,18 +3,19 @@ const router = express.Router();
 const controller = require("./../../controllers/users");
 const authentication = require ("./../../middlewares/authentication");
 const authorization = require ("./../../middlewares/authorization");
+const audits = require ("./../../middlewares/audits");
 
 router.route("/")
     .post(controller.newUser)
-    .get(authentication, controller.getUsers);
+    .get(authentication, audits, controller.getUsers);
 
 router.route("/login")
     .post(controller.login)
 
 router.route("/:username")
     .get(authentication, controller.getUser)
-    .put(authentication, authorization, controller.updateUser)
-    .delete(authentication, authorization, controller.deleteUser);
+    .put(authentication, authorization, audits, controller.updateUser)
+    .delete(authentication, authorization, audits, controller.deleteUser);
 
 
 

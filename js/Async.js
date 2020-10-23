2493c1d85b933b19c4d770aed976157b12b5d37e
Jefferson <jefry04@gmail.com>
2020-10-22 20:19:01 -0500

    async await

---
+++ js/Async.js
@@ -0,0 +1,65 @@
+const tasks = [
+    {id: 1, name:"desayunar", user: "jefry04"}, 
+    {id: 1, name:"banarse", user: "jefry04"}, 
+    {id: 1, name:"trabajarr", user: "jefry04"}, 
+    {id: 1, name:"cenarr", user: "jefry04"}, 
+]
+//var  getTasks = (arr) => {
+//    setTimeout(()=> {console.log(arr);}, 3000); // retrasp 3s el console
+//}
+
+
+// Promesa
+/*var getTasks = (arr) => {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => { resolve(arr) }, 3000); 
+    }); 
+};
+
+getTasks(tasks)
+.then((tasksArray)=>{
+    console.log(tasksArray); 
+});*/
+
+/*
+// incluyendo un reject (error) cuando el array esta vacio
+var getTasks = (arr) => {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => { 
+            arr.length > 0 ? 
+                resolve(arr) 
+            : 
+                reject("no hay datos"); 
+        }, 3000); 
+    }); 
+};
+
+getTasks([]) // mando array vacio
+.then((tasksArray)=>{
+    console.log(tasksArray); 
+})
+.catch((err)=>{
+    console.log(err); 
+});
+console.log("Se ejecutó la línea después del llamado de la función getTasks");
+*/
+
+//async await
+var getTasks = (arr) => {
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => { 
+            arr.length > 0 ? 
+                resolve(arr) 
+            : 
+                reject("no hay datos"); 
+        }, 3000); 
+    }); 
+};
+
+const getAsyncTasks = async () => {
+    const tasksArray = await getTasks(tasks);
+    console.log(tasksArray);
+    console.log("Se ejecutó la línea después del llamado de la función getTasks");
+}
+getAsyncTasks();
+

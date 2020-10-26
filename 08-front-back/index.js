d613a9634b33c11ba59ccd809874967a1aefe7df
Jefferson <jefry04@gmail.com>
2020-10-25 20:03:34 -0500

    cors + express static

--- 08-front-back/index.js
+++ 08-front-back/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const mongoose = require ("mongoose");
 const express = require("express");
+const cors = require ("cors");
 const app = express(); 
+
 const config = require("./config");
 const api = require ("./api");
 
+app.use(express.static("public"));
+app.use(cors());
 app.use(express.json());  
 app.use("/api", api);
 app.use("/api/v1", api);
